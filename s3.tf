81d5e51ee6936c72847342574c3aea6eb083fa1f
PeterGrecian <peter.grecian@gmail.com>
2025-07-22 13:51:42 +0100

    create buckets

--- s3.tf
+++ s3.tf
@@ -5,3 +5,11 @@ resource "aws_s3_bucket" "bucketA" {
 resource "aws_s3_bucket" "bucketB" {
   bucket = "gela602ca3-bucket-b"
 }
+
+output "bucketA_name" {
+  value = aws_s3_bucket.bucketA.id
+}
+
+output "bucketB_name" {
+  value = aws_s3_bucket.bucketB.id
+}
