09adc3b5cbbd8a84bd9166a0b5c84256434a3862
PeterGrecian <peter.grecian@gmail.com>
2025-07-23 19:18:18 +0100

    utils-terraform

--- gitinfo.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-branch=$(git rev-parse --abbrev-ref HEAD)
-remote=$(git config --get remote.origin.url)
-commit=$(git log -1 --oneline | awk '{print $1}')
-
-echo "{\"gitinfo\" : \"$remote $branch $commit\"}"
